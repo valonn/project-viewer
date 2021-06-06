5b1f6288dfe9bdf5caffe21c721250f8c575f8b6
Valon Kito <valon.kito@gmail.com>
2021-06-06 22:14:29 +0200

    fixes

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const app = express();
 app.use(requireHTTPS);
 app.use(express.static('./dist/project-viewer'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/project-viewer/'}),
+app.get('/en/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/project-viewer/en-US'}),
+);
+
+
+app.get('/sq/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/project-viewer/sq'}),
 );
 
 app.listen(process.env.PORT || 8080);
